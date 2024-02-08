4f0e3a9da82f1547a17b422587ab2b5bc2ac6593
anne04 <anne.06.cse@gmail.com>
2024-02-08 09:44:19 -0500

    Create dot_to_pdf_svg.sh

---
+++ dot_to_pdf_svg.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+if [$1 = '']
+then
+	echo "Please supply the dot file name (along with path) to convert. Program ending."
+
+else
+	echo "converting to a temporary tmp file"
+	cat $1 | sed 's/L:\([^ ]\+\), R:/\1-/g'   | sed 's/label="[0-9][^"]*"/label=""/g' | awk -F'=' '{ if ($1 == "penwidth") {print $1 "=" ($2 ^ 6) ","} else {print $0 }}'   | tr '\n' ' '   | sed "s/;/\n/g"  > tmp
+
+	echo "converting to pdf file"
+	cat tmp   | dot -Kneato -n -y -Tsvg -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Npenwidth=20 -Epenwidth=20 -Nstyle="filled"> test.svg
+
+	echo "converting to a svg file"
+	cat tmp   | dot -Kneato -n -y -Tpdf -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Npenwidth=20 -Epenwidth=20 -Nstyle="filled"> test.pdf
+
+	echo "Deleting the temporary temp file"
+	rm tmp
+  
+
+fi
+
+
