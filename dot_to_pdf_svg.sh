c6277d6dbb8f84497780117e7f2c4ff90b78454b
anne04 <anne.06.cse@gmail.com>
2024-04-08 13:27:20 -0400

    removed -Npenwidth=20

--- dot_to_pdf_svg.sh
+++ dot_to_pdf_svg.sh
@@ -10,10 +10,10 @@ else
 	cat $1 | sed 's/L:\([^ ]\+\), R:/\1-/g'   | sed 's/label="[0-9][^"]*"/label=""/g' | awk -F'=' '{ if ($1 == "penwidth") {print $1 "=" ($2 ^ 6) ","} else {print $0 }}'   | tr '\n' ' '   | sed "s/;/\n/g"  > tmp
 
 	echo "converting to pdf file"
-	cat tmp   | dot -Kneato -n -y -Tsvg -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Npenwidth=20 -Epenwidth=20 -Nstyle="filled"> edge_graph.svg
+	cat tmp   | dot -Kneato -n -y -Tsvg -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Epenwidth=20 -Nstyle="filled"> edge_graph.svg #-Npenwidth=20 
 
 	echo "converting to a svg file"
-	cat tmp   | dot -Kneato -n -y -Tpdf -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Npenwidth=20 -Epenwidth=20 -Nstyle="filled"> edge_graph.pdf
+	cat tmp   | dot -Kneato -n -y -Tpdf -Efontname="Arial" -Nlabel="" -Nwidth=1 -Nheight=1 -Epenwidth=20 -Nstyle="filled"> edge_graph.pdf #-Npenwidth=20 
 
 	echo "Deleting the temporary temp file"
 	rm tmp
