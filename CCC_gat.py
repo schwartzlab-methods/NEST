322a0fd5caa5d84f91db0d32951f1d29b985950d
anne04 <anne.06.cse@gmail.com>
2024-05-30 17:56:31 -0400

    Update CCC_gat.py with loading last model state

--- CCC_gat.py
+++ CCC_gat.py
@@ -142,108 +142,125 @@ def train_NEST(args, data_loader, in_channels):
         encoder=Encoder(in_channels=in_channels, hidden_channels=args.hidden, heads=args.heads, dropout = args.dropout),
         summary=lambda z, *args, **kwargs: torch.sigmoid(z.mean(dim=0)),
         corruption=corruption).to(device)
+    
     #print('initialized DGI model')
-    #print(DGI_model.encoder.attention_scores_mine)
     #DGI_optimizer = torch.optim.Adam(DGI_model.parameters(), lr=0.005, weight_decay=5e-4)
-    DGI_optimizer = torch.optim.Adam(DGI_model.parameters(), lr=args.lr_rate) #1e-5)#5 #6
-    #DGI_optimizer = torch.optim.RMSprop(DGI_model.parameters(), lr=1e-5)
+    DGI_optimizer = torch.optim.Adam(DGI_model.parameters(), lr=args.lr_rate) #1e-5)#5 #6 #DGI_optimizer = torch.optim.RMSprop(DGI_model.parameters(), lr=1e-5)
     DGI_filename = args.model_path+'DGI_'+ args.model_name  +'.pth.tar'
 
-    if args.load:
-        DGI_load_path = args.model_path+'DGI_'+ args.load_model_name+'.pth.tar'
-        DGI_model.load_state_dict(torch.load(DGI_load_path))
-        DGI_optimizer.load_state_dict(torch.load(args.model_path+'DGI_optimizer_'+ args.load_model_name  +'.pth.tar'))
-
-
+    if args.load == 1:
+        print('loading model')
+        checkpoint = torch.load(DGI_filename)
+        DGI_model.load_state_dict(checkpoint['model_state_dict'])
+        DGI_model.to(device)
+        DGI_optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch_start = checkpoint['epoch']
+        min_loss = checkpoint['loss']
+        '''
+        for state in DGI_optimizer.state.values():
+            for k, v in state.items():
+                if isinstance(v, torch.Tensor):
+                    state[k] = v.to(device)
+        ''' 
+        print('min_loss was %g'%min_loss)
     else:
-        import datetime
-        start_time = datetime.datetime.now()
-        min_loss=10000
         print('Saving init model state ...')
-        torch.save(DGI_model.state_dict(), args.model_path+'DGI_init'+ args.model_name  + '.pth.tar')
-        torch.save(DGI_optimizer.state_dict(), args.model_path+'DGI_optimizer_init'+ args.model_name  + '.pth.tar')
-        #print('training starts ...')
-        for epoch in range(args.num_epoch):
-            DGI_model.train()
-            DGI_optimizer.zero_grad()
-            DGI_all_loss = []
+        torch.save({
+            'epoch': 0,
+            'model_state_dict': DGI_model.state_dict(),
+            'optimizer_state_dict': DGI_optimizer.state_dict(),
+            #'loss': loss,
+            }, args.model_path+'DGI_init_model_optimizer_'+ args.model_name  + '.pth.tar')
+        min_loss = 10000
+        epoch_start = 0
+        
+    import datetime
+    start_time = datetime.datetime.now()
 
-            for data in data_loader:
-                data = data.to(device)
-                pos_z, neg_z, summary = DGI_model(data=data)
-                DGI_loss = DGI_model.loss(pos_z, neg_z, summary)
-                DGI_loss.backward()
-                DGI_all_loss.append(DGI_loss.item())
-                DGI_optimizer.step()
+    #print('training starts ...')
+    for epoch in range(epoch_start, args.num_epoch):
+        DGI_model.train()
+        DGI_optimizer.zero_grad()
+        DGI_all_loss = []
 
-            if ((epoch)%500) == 0:
-                print('Epoch: {:03d}, Loss: {:.4f}'.format(epoch+1, np.mean(DGI_all_loss)))
-                loss_curve[loss_curve_counter] = np.mean(DGI_all_loss)
-                loss_curve_counter = loss_curve_counter + 1
+        for data in data_loader:
+            data = data.to(device)
+            pos_z, neg_z, summary = DGI_model(data=data)
+            DGI_loss = DGI_model.loss(pos_z, neg_z, summary)
+            DGI_loss.backward()
+            DGI_all_loss.append(DGI_loss.item())
+            DGI_optimizer.step()
 
-                if np.mean(DGI_all_loss)<min_loss:
+        if ((epoch)%500) == 0:
+            print('Epoch: {:03d}, Loss: {:.4f}'.format(epoch+1, np.mean(DGI_all_loss)))
+            loss_curve[loss_curve_counter] = np.mean(DGI_all_loss)
+            loss_curve_counter = loss_curve_counter + 1
 
-                    min_loss=np.mean(DGI_all_loss)
+            if np.mean(DGI_all_loss)<min_loss:
 
-                    # save the current model state
-                    torch.save(DGI_model.state_dict(), DGI_filename)
-                    torch.save(DGI_optimizer.state_dict(), args.model_path+'DGI_optimizer_'+ args.model_name  +'.pth.tar')
-                    save_tupple=[pos_z, neg_z, summary] 
+                min_loss=np.mean(DGI_all_loss)
 
-                    # save the node embedding
-                    X_embedding = pos_z
-                    X_embedding = X_embedding.cpu().detach().numpy()
-                    X_embedding_filename =  args.embedding_path + args.model_name + '_Embed_X' #.npy
-                    with gzip.open(X_embedding_filename, 'wb') as fp:  
-                        pickle.dump(X_embedding, fp)
-                        
-                    #np.save(X_embedding_filename, X_embedding) #/cluster/home/t116508uhn/.local/lib/python3.7/site-packages/numpy/lib/npyio.py:528: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. If you meant to do this, you must specify 'dtype=object' when creating the ndarray.
-                    
-                    # save the attention scores
+                ######## save the current model state ########
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': DGI_model.state_dict(),
+                    'optimizer_state_dict': DGI_optimizer.state_dict(),
+                    'loss': min_loss,
+                    }, DGI_filename)
 
-                    X_attention_index = DGI_model.encoder.attention_scores_mine[0]
-                    X_attention_index = X_attention_index.cpu().detach().numpy()
+                ##################################################
+                # save the node embedding
+                X_embedding = pos_z
+                X_embedding = X_embedding.cpu().detach().numpy()
+                X_embedding_filename =  args.embedding_path + args.model_name + '_Embed_X' #.npy
+                with gzip.open(X_embedding_filename, 'wb') as fp:  
+                    pickle.dump(X_embedding, fp)
+                                    
+                # save the attention scores
+                X_attention_index = DGI_model.encoder.attention_scores_mine[0]
+                X_attention_index = X_attention_index.cpu().detach().numpy()
 
-                    # layer 1
-                    X_attention_score_normalized_l1 = DGI_model.encoder.attention_scores_mine_l1[1]
-                    X_attention_score_normalized_l1 = X_attention_score_normalized_l1.cpu().detach().numpy()
-                    # layer 1 unnormalized
-                    X_attention_score_unnormalized_l1 = DGI_model.encoder.attention_scores_mine_unnormalized_l1
-                    X_attention_score_unnormalized_l1 = X_attention_score_unnormalized_l1.cpu().detach().numpy()
+                # layer 1
+                X_attention_score_normalized_l1 = DGI_model.encoder.attention_scores_mine_l1[1]
+                X_attention_score_normalized_l1 = X_attention_score_normalized_l1.cpu().detach().numpy()
+                # layer 1 unnormalized
+                X_attention_score_unnormalized_l1 = DGI_model.encoder.attention_scores_mine_unnormalized_l1
+                X_attention_score_unnormalized_l1 = X_attention_score_unnormalized_l1.cpu().detach().numpy()
 
-                    # layer 2
-                    X_attention_score_normalized = DGI_model.encoder.attention_scores_mine[1]
-                    X_attention_score_normalized = X_attention_score_normalized.cpu().detach().numpy()
-                    # layer 2 unnormalized
-                    X_attention_score_unnormalized = DGI_model.encoder.attention_scores_mine_unnormalized
-                    X_attention_score_unnormalized = X_attention_score_unnormalized.cpu().detach().numpy()
+                # layer 2
+                X_attention_score_normalized = DGI_model.encoder.attention_scores_mine[1]
+                X_attention_score_normalized = X_attention_score_normalized.cpu().detach().numpy()
+                # layer 2 unnormalized
+                X_attention_score_unnormalized = DGI_model.encoder.attention_scores_mine_unnormalized
+                X_attention_score_unnormalized = X_attention_score_unnormalized.cpu().detach().numpy()
 
-                    print('making the bundle to save')
-                    X_attention_bundle = [X_attention_index, X_attention_score_normalized_l1, X_attention_score_unnormalized, X_attention_score_unnormalized_l1, X_attention_score_normalized]
-                    X_attention_filename =  args.embedding_path + args.model_name + '_attention' #.npy
-                    # np.save(X_attention_filename, X_attention_bundle) # this is deprecated
-                    with gzip.open(X_attention_filename, 'wb') as fp:  
-                        pickle.dump(X_attention_bundle, fp)
+                print('making the bundle to save')
+                X_attention_bundle = [X_attention_index, X_attention_score_normalized_l1, X_attention_score_unnormalized, X_attention_score_unnormalized_l1, X_attention_score_normalized]
+                X_attention_filename =  args.embedding_path + args.model_name + '_attention' #.npy
+                # np.save(X_attention_filename, X_attention_bundle) # this is deprecated
+                with gzip.open(X_attention_filename, 'wb') as fp:  
+                    pickle.dump(X_attention_bundle, fp)
 
-                    logfile=open(args.model_path+'DGI_'+ args.model_name+'_loss_curve.csv', 'wb')
-                    np.savetxt(logfile,loss_curve, delimiter=',')
-                    logfile.close()
+                logfile=open(args.model_path+'DGI_'+ args.model_name+'_loss_curve.csv', 'wb')
+                np.savetxt(logfile,loss_curve, delimiter=',')
+                logfile.close()
 
-                    #print(DGI_model.encoder.attention_scores_mine_unnormalized_l1[0:10])
+                #print(DGI_model.encoder.attention_scores_mine_unnormalized_l1[0:10])
 
 #            if ((epoch)%60000) == 0:
 #                DGI_optimizer = torch.optim.Adam(DGI_model.parameters(), lr=1e-6)  #5 #6
 
-        end_time = datetime.datetime.now()
+    end_time = datetime.datetime.now()
 
-#        torch.save(DGI_model.state_dict(), DGI_filename)
-        print('Training time in seconds: ', (end_time-start_time).seconds)
-        DGI_model.load_state_dict(torch.load(DGI_filename))
-        print("debug loss")
-        DGI_loss = DGI_model.loss(pos_z, neg_z, summary)
-        print("debug loss latest tupple %g"%DGI_loss.item())
-        DGI_loss = DGI_model.loss(save_tupple[0], save_tupple[1], save_tupple[2])
-        print("debug loss min loss tupple %g"%DGI_loss.item())
+    print('Training time in seconds: ', (end_time-start_time).seconds)
+
+    checkpoint = torch.load(DGI_filename)
+    DGI_model.load_state_dict(checkpoint['model_state_dict'])
+    DGI_model.to(device)
+    DGI_model.eval()
+    print("debug loss")
+    DGI_loss = DGI_model.loss(pos_z, neg_z, summary)
+    print("debug loss latest tupple %g"%DGI_loss.item())
 
     return DGI_model
 
