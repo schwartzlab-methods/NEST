a52c8611571cc1af7489147bdaac0ce2c3988de2
anne04 <anne.06.cse@gmail.com>
2024-06-23 09:27:27 -0400

    Update run_NEST.py

--- run_NEST.py
+++ run_NEST.py
@@ -30,16 +30,37 @@ if __name__ == "__main__":
     parser.add_argument( '--lr_rate', type=float, default=0.00001)
     parser.add_argument( '--manual_seed', type=str, default='no')
     parser.add_argument( '--seed', type=int )
-    parser.add_argument( '--total_subgraphs', type=int, default=1) # if split, then more than 1
+    #parser.add_argument( '--split', type=int, default=0)
+    parser.add_argument( '--total_subgraphs', type=int, default=1)
+    parser.add_argument( '--metadata_to', type=str, default='metadata/', help='Path to save the metadata')
     #=========================== optional ======================================
     parser.add_argument( '--load', type=int, default=0, help='Load a previously saved model state')  
+    parser.add_argument( '--load_model_name', type=str, default='None' , help='Provide the model name that you want to reload')
     #============================================================================
     args = parser.parse_args() 
 
+    #parser.add_argument( '--options', type=str)
+    #parser.add_argument( '--withFeature', type=str, default='r1') 
+    #parser.add_argument( '--workflow_v', type=int, default=1)
+    #parser.add_argument( '--datatype', type=str)
+
+    '''
+    if args.total_subgraphs > 1 :
+        args.training_data = args.training_data + args.data_name + '/' + args.data_name + '_' + 'graph_bag'
+    else:
+        args.training_data = args.training_data + args.data_name + '/' + args.data_name + '_' + 'adjacency_records'
+    '''
+
     args.training_data = args.training_data + args.data_name + '/' + args.data_name + '_' + 'adjacency_records'
+
+    if args.total_subgraphs > 1 :
+        node_id_sorted = args.metadata_to + args.data_name + '/'+ args.data_name+'_'+'node_id_sorted_xy'
+
     args.embedding_path = args.embedding_path + args.data_name +'/'
     args.model_path = args.model_path + args.data_name +'/'
     args.model_name = args.model_name + '_r' + str(args.run_id)
+
+
 
     print(args.data_name+', '+str(args.heads)+', '+args.training_data+', '+str(args.hidden) )
 
@@ -47,9 +68,6 @@ if __name__ == "__main__":
         torch.manual_seed(args.seed)
         random.seed(args.seed)
         np.random.seed(args.seed)
-        
-    if args.total_subgraphs > 1 :
-        node_id_sorted = args.metadata_to + args.data_name + '/'+ args.data_name+'_'+'node_id_sorted_xy'
 
 
     if not os.path.exists(args.embedding_path):
@@ -63,7 +81,6 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
 
-
     if args.total_subgraphs == 1:
         from CCC_gat import get_graph, train_NEST
         # data preparation
@@ -72,8 +89,9 @@ if __name__ == "__main__":
         DGI_model = train_NEST(args, data_loader=data_loader, in_channels=num_feature)
         # training done
     elif args.total_subgraphs > 1:
-        from CCC_gat_split import get_split_graph, train_NEST
+        from CCC_gat_split import get_split_graph, train_NEST #_v2
         # data preparation
+        # graph_bag, num_feature = get_graph(args.training_data)
         graph_bag, num_feature = get_split_graph(args.training_data, node_id_sorted, args.total_subgraphs)    
         # train the model
         DGI_model = train_NEST(args, graph_bag=graph_bag, in_channels=num_feature)
